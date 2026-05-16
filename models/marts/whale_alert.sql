4569bb707efc7472156e8e03961cd605181c3db5
Jerry k paul <pauljerryk@gmail.com>
2026-05-16 19:01:46 +0530

    whale alert produced

---
+++ models/marts/whale_alert.sql
@@ -0,0 +1,23 @@
+with whale_alert as (
+
+select
+output_address,
+sum(output_value) as total_sent,
+count(*)as tx_count
+
+from {{ ref('stg_btc_transactions') }}
+where output_value > 10
+
+group by output_address
+order by total_sent desc
+
+)
+
+select
+w.output_address,
+w.total_sent,
+w.tx_count,
+{{ convert_to_usd('w.total_sent') }} as total_sent_usd
+
+from whale_alert w
+order by total_sent desc
